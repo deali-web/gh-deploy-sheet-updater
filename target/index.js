dc4484647cb54b995b2cb289a149140f7abd7a68
이다빈 <dabin@deali.net>
2025-03-09 20:24:24 +0900

    기본값 추가

--- target/index.js
+++ target/index.js
@@ -37,17 +37,16 @@ const core = __importStar(require("@actions/core"));
 const googleSheets_1 = require("./googleSheets");
 async function run() {
     try {
-        const project = core.getInput("project") || "";
-        const environment = core.getInput("environment") || "";
-        const branch = process.env.GITHUB_REF_NAME || ""; // 실행 중인 브랜치 가져오기
-        const deployer = process.env.GITHUB_ACTOR || ""; // GitHub Actions 실행한 사용자 가져오기
+        const project = core.getInput("project") || "프로젝트명";
+        const environment = core.getInput("environment") || "실행환경";
+        const branch = process.env.GITHUB_REF_NAME || "배포 브랜치"; // 실행 중인 브랜치 가져오기
+        const deployer = process.env.GITHUB_ACTOR || "배포자"; // GitHub Actions 실행한 사용자 가져오기
         const spreadsheetId = process.env.SPREADSHEET_ID ||
             "1nOstjlAkVG0hSLJbcOkePzde2c2Oa-JzGQG87wqodBM";
         if (!spreadsheetId) {
             throw new Error("SPREADSHEET_ID가 설정되지 않았습니다.");
         }
         await (0, googleSheets_1.updateGoogleSheet)(spreadsheetId, project, environment, branch, deployer);
-        core.info("✅ Google Sheets 업데이트 완료!");
     }
     catch (error) {
         core.setFailed(`❌ 작업 실패: ${error.message}`);
